8759bc453d82d8d0da9a1901a1e0d30869c9356c
Darío Medina <dariomedina10@gmail.com>
2018-10-09 16:48:00 -0400

    rutinas para insertar tipo de empleado

---
+++ Control_Asistencia_Webform/ingresar_tipo_empleado.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Control_Asistencia_Webform
+{
+    public partial class ingresar_tipo_empleado : System.Web.UI.Page
+    {
+        public bool IsPostback { get; private set; }
+        int valor;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostback)
+            {
+                Label1.Visible = false;
+            }
+        }
+
+
+
+        //evento del botón
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection Conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ToString());
+                {
+                    Label1.Visible = true;
+                    SqlCommand cmd = new SqlCommand("INSERT_tipo_empleado", Conexion);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@id_tipo_empl", SqlDbType.Int).Value = 0;
+                    cmd.Parameters.Add("@descr", System.Data.SqlDbType.VarChar).Value = descripcion.Text;
+                    Conexion.Open();
+                    cmd.ExecuteNonQuery();
+                    Conexion.Close();
+                    Label1.Text = "Tipo de Empleado Insertado Exitosamente";
+                }
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "Tipo de Empleado No Insertado" + ex.Message;
+            }
+        }
+
+
+    }
+
+
+}
