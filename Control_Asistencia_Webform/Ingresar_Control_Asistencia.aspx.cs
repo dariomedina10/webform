308958200c1c0b06e9d9fb40d31adc71e3ca360e
Darío Medina <dariomedina10@gmail.com>
2018-10-11 10:00:35 -0400

    Inicio rutinas insercion control_asistencia

---
+++ Control_Asistencia_Webform/Ingresar_Control_Asistencia.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Control_Asistencia_Webform
+{
+    public partial class Ingresar_Control_Asistencia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
