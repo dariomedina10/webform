9f7f104dafadb61e68b1c096e5b414096c354296
Darío Medina <dariomedina10@gmail.com>
2018-10-15 14:02:06 -0400

    front end consulta departamentos

---
+++ Control_Asistencia_Webform/Consulta_Departamentos.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Consulta_Departamentos.aspx.cs" MasterPageFile="~/Site.Master" Inherits="Control_Asistencia_Webform.Consulta_Departamentos" %>
+
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+
+      <center>
+     <h2>
+      <body>
+  <p style="color:white";> <strong>Consulta Por Departamentos</strong></p>
+</body>
+
+        
+     </h2>       
+    </center>
+     <form>
+  <div class="form-group">
+      <center>
+         <div class="form-group">
+    <label for="exampleInputPassword1"></label>
+   <%-- <input type="text"  id="clave" placeholder="Ingrese Codigo">--%>
+<asp:TextBox ID="clave" runat="server" placeholder="Ingrese Codigo" ></asp:TextBox>
+             <br />
+             <br />
+             <asp:Button ID="Button2" runat="server" Text="Buscar" Width="97px" OnClick="Button2_Click" /> 
+  </div>
+         
+         <asp:GridView ID="grid_depto" runat="server"  CssClass="table table-hover table-striped" GridLines="None"
+    AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" AllowPaging="True" PageSize="4" OnPageIndexChanging="grid_cargos_PageIndexChanging">
+               <AlternatingRowStyle BackColor="White" />
+               <Columns>
+        <asp:BoundField DataField="id_depto" HeaderText="Código del Departamento" Visible="true" />
+        <asp:BoundField DataField="descripcion" HeaderText="Descripción" />
+       
+    </Columns>
+                 <PagerStyle HorizontalAlign="Center" ForeColor="White" />
+                 </asp:GridView>
+</center>
+ </div>
+
+</form>
+</asp:Content>
+
+
---
+++ Control_Asistencia_Webform/Consulta_Departamentos.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Control_Asistencia_Webform {
+    
+    
+    public partial class Consulta_Departamentos {
+        
+        /// <summary>
+        /// Control clave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox clave;
+        
+        /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
+        /// Control grid_depto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grid_depto;
+    }
+}
